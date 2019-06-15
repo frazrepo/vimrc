f3aab32c5b6a84d6d5b049c0a61a0a2c3e5c8f56
frazrepo <frazvisuals@gmail.com>
2019-06-15 18:39:40 +0200

    Update bat to install nano

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ set userConfig=%USERPROFILE%/\_vimrc
 set backupConfig=%USERPROFILE%/\_vimrc.bkp
 set vimrcfile=vimrc
 set minVimrcfile=minimal-vimrc
+set nanoVimrcfile=nano-vimrc
 
 if exist %backupConfig% (
     del "%backupConfig%"
@@ -17,6 +18,10 @@ if exist %userConfig% (
 if "%1" == "/m" (
     echo "Installing minimal vimrc file.."
     copy /y "%minVimrcfile%" "%userConfig%"
+)
+if "%1" == "/n" (
+    echo "Installing nano vimrc file.."
+    copy /y "%nanoVimrcfile%" "%userConfig%"
 ) else (
     echo "Installing vimrc file.."
     copy /y "%vimrcfile%" "%userConfig%"
